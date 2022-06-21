893d0de00df8f163fa820c3a226849a780ada8ea
Yash Patari <42299664+yashpatari@users.noreply.github.com>
2022-06-21 11:49:52 +0530

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,28 @@
 import React from "react"
 import logo from "../img/logo.png"
-
-export default function Navbar(props){
-    return(
-    <nav className={props.darkMode ? "dark" : ""}>
-        <img 
-            className="nav--logo_icon"
-            src={logo}
-         />
-        <h3 className="nav--logo_text">ReactFacts</h3>
-        <div className="toggler">
-            <p className="toggler--light">Light</p>
-            <div className="toggle--slider" onClick={props.toggleDarkMode}>
-                <div className="toggle--slider--circle"></div>
+export default function Navbar(props) {
+    return (
+         <nav 
+            className={props.darkMode ? "dark": ""}
+        >
+            <img 
+                className="nav--logo_icon"
+                src={logo}
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
             </div>
-            <p className="toggle--dark">Dark</p>
-        </div>
-    </nav>
-)}
+        </nav>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 import App from "./App"
 import ReactDOM from "react-dom"
-
+import "./style.css"
 ReactDOM.render(<App />, document.getElementById("root"))
